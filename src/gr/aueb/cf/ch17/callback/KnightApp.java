30934fe512e540871168d05843d9ac68ad78dcb1
steliostsoupas <stelios-tsoupas@hotmail.com>
2023-06-20 22:02:51 +0300

    added classes on ch17

---
+++ src/gr/aueb/cf/ch17/callback/KnightApp.java
@@ -0,0 +1,27 @@
+package gr.aueb.cf.ch17.callback;
+
+public class KnightApp {
+
+    public static void main(String[] args) {
+        IKnight kingArthur = new Knight();
+        IKnight saitGeorge = new Knight();
+
+        // Lambda Expressions
+//        kingArthur.embarkOnMission(() -> System.out.println("Save the princess"));
+//        saitGeorge.embarkOnMission(() -> killTheDragon());
+
+        // Method References
+        kingArthur.embarkOnMission(KnightApp::killTheDragon);
+        saitGeorge.embarkOnMission(KnightApp::saveThePrincess);
+
+
+    }
+
+    public static void killTheDragon() {
+        System.out.println("Kill the Dragon");
+    }
+
+    public static void saveThePrincess() {
+        System.out.println("Save the Princess");
+    }
+}
